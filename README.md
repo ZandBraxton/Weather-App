30cd89e4c64c17d246da6f0e70aba09e0fe9b05f
ZandBraxton <Zand.Braxton@gmail.com>
2021-09-13 15:27:46 -0500

    Got the data, only need to display it

--- README.md
+++ README.md
@@ -7,13 +7,28 @@ Have search bar to search city
 Have toggle for C and F
 
 display name
+temp
+weather
+max temp/min temp
 
-display Main data
-"Feels like"
-"Humidity"
-"Pressure"
-"Tempt"
+display hourly data (maybe) {
+    time
+    icon
+    temp
+}
 
-display current weather
+display 7 day forecast {
+    render day name
+    render weather icon
+    render humidity (maybe)
+    render chance of rain
+    render high and low temp
+}
 
-display wind
+Sunrise ----- Sunset
+Humidity --- feels like
+chance of rain --- percipitation
+wind speed ---- pressure
+visability ---- uv index
+
+Use both location data 
--- src/index.js
+++ src/index.js
@@ -2,34 +2,50 @@ import './style.css';
 
 
 // `api.openweathermap.org/data/2.5/weather?q=${cityname}&APPID=8da6e4702fef427379c1ed0387c3fc89`
+let units = "imperial"
 
-async function getData(cityname, units) {
+
+async function getLocation(cityname) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=${units}&APPID=8da6e4702fef427379c1ed0387c3fc89`, {mode: 'cors'})
+    const weatherLocation = await response.json()
+    renderCityName(weatherLocation.name)
+    getData(weatherLocation.coord['lat'], weatherLocation.coord['lon'] )
+}
+
+async function getData(lat, lon) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&appid=8da6e4702fef427379c1ed0387c3fc89`, {mode: 'cors'})
     const weatherData = await response.json()
-    console.log(weatherData.name)
-    console.log(weatherData.main)
-    console.log(weatherData.weather[0]['main'])
-    console.log(weatherData.wind)
-    renderData(weatherData)
+    console.log(weatherData)
+    renderHeaderData(weatherData)
 }
 
 
 
- function renderData(data) {
-      const name = document.querySelector('.name')
-      name.textContent = data.name
+function renderCityName(locationname) {
+    const name = document.querySelector('.name')
+    name.textContent = locationname
+}
 
-      const currentWeather = document.querySelector('.current-weather')
-      currentWeather.textContent = data.weather[0]['main']
+function renderHeaderData(data) {
+    const currentWeather = document.querySelector('.current-weather')
+    currentWeather.textContent = data.current.weather[0]['main']
 
-      const maxTemp = document.querySelector('#maxTemp')
-      maxTemp.textContent = `H:${Math.round(data.main['temp_max'])}\xB0`
+    const temp = document.querySelector('.temp')
+    temp.textContent = `${Math.round(data.current['temp'])}\xB0`
 
-      const minTemp = document.querySelector('#minTemp')
-      minTemp.textContent = `L:${Math.round(data.main['temp_min'])}\xB0`
+    const maxTemp = document.querySelector('#maxTemp')
+    maxTemp.textContent = `H:${Math.round(data.daily[0].temp['max'])}\xB0`
 
-  }
+    const minTemp = document.querySelector('#minTemp')
+    minTemp.textContent = `L:${Math.round(data.daily[0].temp['min'])}\xB0`
+}
 
-  getData("San-Antonio", "imperial").catch(err => {
-    console.log(err)
-  })
+function renderMainData(data) {
+
+}
+
+getLocation("San-Antonio", "imperial").catch(err => {
+console.log(err)
+})
+
+//   https://api.openweathermap.org/data/2.5/onecall?lat=29.4241&lon=-98.4936&exclude=minutely&units=imperial&appid=8da6e4702fef427379c1ed0387c3fc89
