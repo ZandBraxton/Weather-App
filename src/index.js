55eafaca241d08537618076653f96a0432b1dca5
ZandBraxton <Zand.Braxton@gmail.com>
2021-09-14 14:07:21 -0500

    Logged more data

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 import './style.css';
 
-
 // `api.openweathermap.org/data/2.5/weather?q=${cityname}&APPID=8da6e4702fef427379c1ed0387c3fc89`
 let units = "imperial"
 
 
+
+const search = document.querySelector('.search')
+search.addEventListener('submit', (e) => {
+    e.preventDefault()
+    getLocation(search.elements[0].value, units)
+})
+
+
+
 async function getLocation(cityname) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=${units}&APPID=8da6e4702fef427379c1ed0387c3fc89`, {mode: 'cors'})
+    if (response.status == 404) {
+        //throw some error here for user to see they didn't put a city name
+        return;
+    }
     const weatherLocation = await response.json()
-    renderCityName(weatherLocation.name)
-    getData(weatherLocation.coord['lat'], weatherLocation.coord['lon'] )
+    getData(weatherLocation)
 }
 
-async function getData(lat, lon) {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&appid=8da6e4702fef427379c1ed0387c3fc89`, {mode: 'cors'})
+async function getData(location) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${location.coord['lat']}&lon=${location.coord['lon']}&units=${units}&appid=8da6e4702fef427379c1ed0387c3fc89`, {mode: 'cors'})
     const weatherData = await response.json()
     console.log(weatherData)
+    renderCityName(location.name)
     renderHeaderData(weatherData)
+    renderMainData(weatherData)
 }
 
 
@@ -42,9 +55,52 @@ function renderHeaderData(data) {
 
 function renderMainData(data) {
 
+    const sunrise = document.querySelector('#sunrise')
+    let sunriseTime = new Date(data.current.sunrise *1000)
+    console.log(sunriseTime)
+    let sunriseHour = sunriseTime.getHours() % 12 || 12
+    let sunriseMinutes = sunriseTime.getMinutes()
+    if (sunriseMinutes < 10) {
+        sunriseMinutes = "0" + sunriseMinutes
+    }
+    sunrise.textContent = `Sunrise: ${sunriseHour}:${sunriseMinutes}AM`
+
+    const sunset = document.querySelector('#sunset')
+    let sunsetTime = new Date(data.current.sunset *1000)
+    let sunsetHour = sunsetTime.getHours() % 12 || 12
+    let sunsetMinutes = sunsetTime.getMinutes()
+    if (sunsetMinutes < 10) {
+        sunsetMinutes = "0" + sunsetMinutes
+    }
+    sunset.textContent = `Sunset: ${sunsetHour}:${sunsetMinutes}PM`
+
+    const humidity = document.querySelector('#humidity')
+    humidity.textContent = `Humidity: ${data.current.humidity}%`
+
+    const feelsLike = document.querySelector('#feels-like')
+    feelsLike.textContent = `Feels Like: ${Math.round(data.current.feels_like)}\xB0`
+
+    const pop = document.querySelector('#pop')
+    pop.textContent = `Chance of Rain: ${data.hourly[0].pop}%`
+
+    const percipitation = document.querySelector('#percipitation')
+    percipitation.textContent = data.daily[0].rain ? `Percipitation: ${data.daily[0].rain} in` : 'Percipitation: 0 in'
+
+    const windspeed = document.querySelector('#windspeed')
+    windspeed.textContent = `Wind: ${data.current.wind_speed} mph`
+
+    const pressure = document.querySelector('#pressure')
+    pressure.textContent = `Pressure: ${data.current.pressure} hPa`
+
+    const visibility = document.querySelector('#visibility')
+    visibility.textContent = ` Visibility: ${data.current.visibility} meters`
+
+    const uvIndex = document.querySelector('#uv-index')
+    uvIndex.textContent = `UV Index: ${data.current.uvi}`
+
 }
 
-getLocation("San-Antonio", "imperial").catch(err => {
+getLocation("San-Antonio", units).catch(err => {
 console.log(err)
 })
 
